2f923845f859ae20a49c6dadc2cddf51d99da4f9
Guilherme Lopes <gui-lopes7@live.com>
2018-07-12 15:36:21 -0300

    aula 56 concluida

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const Post = require('./models/post')
 const app = express();
 
-mongoose.connect("mongodb+srv://gui:CpYXwiuSFgfJ7uK8@cluster0-oy5gg.mongodb.net/node-angular?retryWrites=true")
+mongoose.connect("mongodb+srv://gui:CpYXwiuSFgfJ7uK8@cluster0-oy5gg.mongodb.net/node-angular?retryWrites=true", {useNewUrlParser: true})
   .then(() => {
     console.log('Connected to database!');
   })
@@ -44,7 +44,7 @@ app.get("/api/posts", (req, res, next) => {
 
 app.delete("/api/posts/:id", (req, res, next) => {
   Post.deleteOne({_id: req.params.id}).then(result => {
-    consolelog(result);
+    console.log(result);
     res.status(200).json({ message: "Post deleted!"});
   });
 });
