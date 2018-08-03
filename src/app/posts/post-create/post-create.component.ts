be6ded2e15c68a01627cd1c46d738b1e3f7e8be0
Guilherme Lopes <gui-lopes7@live.com>
2018-08-03 11:50:45 -0300

    aula 73 concluida, upload inicial da imagem

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -27,7 +27,8 @@ export class PostCreateComponent implements OnInit {
       'title': new FormControl(null, {
         validators: [Validators.required, Validators.minLength(3)]}),
       'content': new FormControl(null, {
-        validators: [Validators.required]})
+        validators: [Validators.required]}),
+      'image': new FormControl(null, {validators: [Validators.required]})
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')) {
@@ -41,13 +42,22 @@ export class PostCreateComponent implements OnInit {
             title: postData.title,
             content: postData.content
           };
-          this.form.setValue({'title': this.post.title, 'content': this.post.content});
+          this.form.setValue({
+          'title': this.post.title,
+          'content': this.post.content});
         });
       } else {
         this.mode = 'create';
         this.postId = null;
       }
     });
+  }
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+
   }
 
   onSavePost() {
